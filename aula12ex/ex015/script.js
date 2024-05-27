6300035020d6db3ddddcddcf2a069290d487d23c
Atyla Yuri Torres da Silva <168493753+atylayuri@users.noreply.github.com>
2024-05-27 14:45:33 -0300

    atualizei o verificador de idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,11 +1,49 @@
-function ver() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.querySelector('div#txtano')
-    var res = document.querySelector('div#res')
-     if (fano.value.length == 0 || fano.value > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-     } else {
-        window.alert('Tudo ok')
-     }
+function verificar() {
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if (fano.value == 0 || Number(fano.value > ano)) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!')
+   } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var gênero = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked) {
+         gênero = 'Masculino'
+         if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', 'criança-M.png')
+         } else if (idade < 21) {
+            // Jovem
+            img.setAttribute('src', 'adolescente-M.png')
+         } else if (idade < 50) {
+            // Adulto
+            img.setAttribute('src', 'adulto-M.png')
+         } else {
+            // Idoso
+            img.setAttribute('src', 'idoso-M.png')
+         }
+      } else if (fsex[1].checked) {
+         gênero = 'Femenino'
+         if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', 'criança-F.png')
+         } else if (idade < 21) {
+            // Jovem
+            img.setAttribute('src', 'adolescente-F.png')
+         } else if (idade < 50) {
+            // Adulto
+            img.setAttribute('src', 'adulto-F.png')
+         } else {
+            // Idoso
+            img.setAttribute('src', 'idoso-F.png')
+         }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectamos que é do gênero ${gênero} com ${idade} anos`
+      res.appendChild(img)
+   }
 }
