32992c425f851bf79dc6f7e88865395c38f1cc6b
Atyla Yuri Torres da Silva <168493753+atylayuri@users.noreply.github.com>
2024-05-26 21:12:43 -0300

    verificador de idade

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,11 @@
+function ver() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('div#txtano')
+    var res = document.querySelector('div#res')
+     if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+     } else {
+        window.alert('Tudo ok')
+     }
+}
